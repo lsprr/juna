9f5de13f6e9a4dcd1bfce20d15c0f8f2ae2d077b
lsprr <16653744+lsprr@users.noreply.github.com>
2022-07-02 13:46:16 -0400

    Create several components

---
+++ src/components/Hiscores/Hiscores.js
@@ -0,0 +1,5 @@
+export default function Hiscores() {
+    return (
+        <div>Hi</div>
+    )
+}
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import './Layout.module.css';
+
+export default function Layout(props) {
+    return (
+        <main>
+            {props.children}
+        </main>
+    )
+}
